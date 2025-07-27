909c4bb95b3e707d3cd3e068a126928f6404fced
Foxzie <wolft6977@gmail.com>
2025-07-27 10:39:22 +0700

    DOne

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 // Lazy load components
 const Navbar = lazy(() => import("../components/navbar"));
 const Footer = lazy(() => import("../components/footer"));
-Aos.init();
+
+Aos.init({});
 
 const Button = ({ children, variant = "bg-gray-900", link }) =>
   link ? (
