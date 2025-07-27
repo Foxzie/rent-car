b64aa83371ede1281a7a4bfcee96ebd6bf3b10ed
Foxzie <wolft6977@gmail.com>
2025-07-27 08:35:38 +0700

    Fix Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+      <div className="bg-gray-800 text-white py-4 text-center">
+        <p>© 2025 Andrea's Garage. All rights reserved.</p>
+        <i className="bi bi-list"></i>
+      </div>
+    </>
+  );
+};
+
+export default footer;
