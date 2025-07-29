21444852ea267bf939b33c3f3b5ba15edf819a9e
Foxzie <wolft6977@gmail.com>
2025-07-29 07:09:04 +0700

    Buy

--- src/Brain/new.jsx
+++ src/Brain/new.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Services from "../pages/Services";
